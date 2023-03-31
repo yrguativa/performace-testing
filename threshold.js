b227f0f3079957238e9e99f44f7056a3bae6039c
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-03-30 20:48:42 -0500

    add options to thresholds

--- threshold.js
+++ threshold.js
@@ -5,8 +5,20 @@ export const options = {
     vus: 20,
     duration: "15s",
     thresholds: {
-        http_req_duration: ["p(95)<500"],
+        //http_req_duration: ["p(95)<500"],
         http_req_failed: ["rate<0.35"],
+
+        /**
+         *  Cuando pasado 10s se cumple la condición ( el percentil 95 > 200 ms) se aborta el test
+         *  de lo contrario se continua
+         */
+        http_req_duration: [
+            {
+                threshold: "p(95)<200",
+                abortonFail: true,
+                delayAbortEval: "10s"
+            }
+        ],
     },
 };
 
